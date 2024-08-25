86eee9900fb93f42c1f809f5c50b6bbf1e6102d3
Hasbur bin Hishamuddin <hishamhr@basfad.basf.net>
2024-08-25 16:30:29 +0800

    Refactor: Change the path if productType has children

--- lib/product_portfolio/view/home-care/product_portfolio/home_care_product_portfolio_page.dart
+++ lib/product_portfolio/view/home-care/product_portfolio/home_care_product_portfolio_page.dart
@@ -67,7 +67,20 @@ class _HomeCareProductPortfolioPageState
                           });
                           (productTypeListData[i]['has_children'] as bool)
                               ? context.push(
-                                '${AppRoutes.productPortfolioHomeCare.path}/product/${productTypeListData[i]['category']}/${productTypeListData[i]['product_type_id']}/',
+                                '${AppRoutes.productPortfolioHomeCare.path}/product/${productTypeListData[i]['category']}/${productTypeListData[i]['product_type_id']}/${productTypeListData[i]['product_type_id']}',
+                                  extra: {
+                                    'subtype_id': productTypeListData[i]
+                                            ['product_type_id']
+                                        .toString(),
+                                    'subtype_name': productTypeListData[i]
+                                            ['product_type']
+                                        .toString(),
+                                    'products': productTypeListData[i]
+                                        ['products'],
+                                  },
+                                )
+                              : context.push(
+                                  '${AppRoutes.productPortfolioHomeCare.path}/product/${productTypeListData[i]['category']}/${productTypeListData[i]['product_type_id']}/',
                                   extra: {
                                     'product_type_id': productTypeListData[i]
                                             ['product_type_id']
@@ -79,19 +92,6 @@ class _HomeCareProductPortfolioPageState
                                         productTypeListData[i]
                                                 ['product_type_amount']
                                             .toString(),
-                                  },
-                                )
-                              : context.push(
-                                  '${AppRoutes.productPortfolioHomeCare.path}/product/${productTypeListData[i]['category']}/${productTypeListData[i]['product_type_id']}/${productTypeListData[i]['product_type_id']}',
-                                  extra: {
-                                    'subtype_id': productTypeListData[i]
-                                            ['product_type_id']
-                                        .toString(),
-                                    'subtype_name': productTypeListData[i]
-                                            ['product_type']
-                                        .toString(),
-                                    'products': productTypeListData[i]
-                                        ['products'],
                                   },  
                                 );
                         },
